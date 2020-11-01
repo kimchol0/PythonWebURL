0d1805b7c05e82dd8d666689879fdcd4cbd77e9a
KimChol <kimchol2ece@outlook.com>
2020-11-01 09:01:55 +0800

    加载其它映射文件

--- stu/urls.py
+++ stu/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index_view),
     re_path(r'^query1/(\d{4})/(\d{2})', views.query1_view),
     re_path(r'^query2/(?P<year>[0-9]{4})/(?P<month>[0-9]{2})', views.query2_view)
+    # url(r'^register/', include('stu.urls')),
 ]
