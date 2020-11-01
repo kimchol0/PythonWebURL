4485c96f709a1383adc828d02f4b093f792dabe2
KimChol <kimchol2ece@outlook.com>
2020-11-01 08:45:20 +0800

    传递多个参数

--- stu/urls.py
+++ stu/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_view),
-    re_path(r'^query/(\d{2})', views.query1_view)
+    re_path(r'^query/(\d{4})/(\d{2})', views.query1_view)
 ]
--- stu/views.py
+++ stu/views.py
@@ -7,5 +7,5 @@ def index_view(request):
     return HttpResponse('hello')
 
 
-def query1_view(request, num):
-    return HttpResponse('hello_%s' % num)
+def query1_view(request, year, month):
+    return HttpResponse('hello_%s_%s' % (year, month))
