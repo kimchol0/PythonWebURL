07c5340bf00d93b4615f8dc154d807f03e153343
KimChol <kimchol2ece@outlook.com>
2020-11-01 18:11:58 +0800

    返回协议类型

--- stu2/urls.py
+++ stu2/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, re_path
 from . import views
+
 urlpatterns = [
-    path('', views.index_view),
-    re_path(r'^query1/(\d{4})/(\d{2})', views.query1_view),
+    re_path(r'^$', views.index_view)
 ]
