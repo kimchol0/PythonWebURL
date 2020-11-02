2a1eef787089bf905603d16444f11f80e153791c
KimChol <kimchol2ece@outlook.com>
2020-11-02 20:45:29 +0800

    返回请求实体内容

--- stu2/urls.py
+++ stu2/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    re_path(r'^$', views.index_view)
+    re_path(r'^$', views.index_view),
+    re_path(r'query/$', views.index1_view)
 ]
--- stu2/views.py
+++ stu2/views.py
@@ -5,4 +5,9 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     scheme = request.scheme
-    return HttpResponse('scheme:%s' % scheme)
+    body = request.body
+    return HttpResponse('scheme:%s' % body)
+
+
+def index1_view(request):
+    return render(request, 'index2.html')
